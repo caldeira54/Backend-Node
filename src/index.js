a65e2fd86eebd585d0938d5304870fb663a6fdea
arthur <caldeiraa54@gmail.com>
2023-08-26 17:32:31 -0300

    Adição do cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 const TaskRoutes = require('./routes/TaskRoutes');
 
+server.use(cors());
 server.use(express.json());
 
 server.use('/task', TaskRoutes);
